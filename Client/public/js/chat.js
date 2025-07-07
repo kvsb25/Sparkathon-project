59a745ca24a06f31a51df39ffbde80276a1f493b
Kovidh <kovidhvsbhati@gmail.com>
2025-07-07 11:47:42 +0530

    fix chat.js

--- Client/public/js/chat.js
+++ Client/public/js/chat.js
@@ -1,3 +1,5 @@
+// const eventSource = new EventSource("https://localhost:8080");
+let answering = false;
 const select = document.querySelector.bind(document);
 const chat = select(".chat");
 const textArea = select(".textareaMessage");
@@ -30,23 +32,49 @@ const utils = {
     addUserInputMsg: function () {
         utils.addMsg(consts.role.user, textArea.value);
     },
-    setBtnState: function(disabled){
-        if(sendBtn){
+    addBotMsg: function(){
+        
+    },
+    setBtnState: function (disabled) {
+        if (sendBtn) {
             sendBtn.disable = disabled;
         }
-    }``
+    },
 }
 
 /************ Event Handlers ************/
 
+/***** DOM Handlers *****/
 sendBtn.addEventListener('click', (event) => {
+    if (answering) {
+        return;
+    }
     utils.addUserInputMsg();
-})
+    utils.setBtnState(true);  // enable the button after receiving the full response of bot from the server
+});
 
-textArea.addEventListener("keypress", (event)=>{
-    if(event.code == "Enter" || event.keyCode == 13){
+textArea.addEventListener("keypress", (event) => {
+    if (event.code == "Enter" || event.keyCode == 13) {
+        if (answering) {
+            return;
+        }
         event.preventDefault();
         utils.addUserInputMsg();
         textArea.value = null;
     }
 });
+
+/***** EventSource Handlers *****/
+
+// eventSource.onopen = function(event){
+//     console.log('Successfully connected to the server');
+// }
+
+// eventSource.onmessage = function(event){
+//     const data = JSON.parse(event.data);
+//     console.log(data);
+// }
+
+// eventSource.onerror = function() {
+//     eventSource.close();
+// }
